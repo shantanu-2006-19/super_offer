c20c4458e0572906930f270cc7771fa3634f00a9
shantanu-2006-19 <chavreshantanu2006@gmail.com>
2026-03-12 19:45:22 +0530

    double remove link

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://chavreshantanu011_db_user:Pt3VnVHiH4MlbrFU@cluster0.lrbim8x.mongodb.net/?appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
