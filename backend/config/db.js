d98885dd9b0eab137e5736f8e6cb5330c3e9ecae
shantanu-2006-19 <chavreshantanu2006@gmail.com>
2026-03-12 22:00:15 +0530

    Add logging for MongoDB connection URL and errors
    Log the MongoDB connection URL and error messages.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URL);
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
+    console.log(error.message);
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
