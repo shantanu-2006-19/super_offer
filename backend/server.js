1aec94f6b719f32c3d4b3ab47073ac47305d88a4
shantanu-2006-19 <chavreshantanu2006@gmail.com>
2026-03-12 21:49:57 +0530

    Update CORS configuration to allow multiple origins

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,10 @@ app.use(cookieParser());
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  origin: [process.env.FRONTEND_URL, "https://super-offer-nine.vercel.app" ,'http://localhost:5173'],
+  credentials: true,
+  allowedOrigins: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 // Rate limiting
